066f236819383a2b71237af66fd93ab5e17041da
lsh9955 <cqe9933@gmail.com>
2023-01-18 16:26:01 +0900

    2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { v4: uuidV4 } = require('uuid');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+router.get('/:room', (req, res) => {
+  res.render('index', { roomId: req.params.room });
 });
 
 module.exports = router;
